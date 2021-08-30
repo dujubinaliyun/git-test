e0b28c7f88a185c17705e20f4e2348bec56fef84
dujubinaliyun <89722346+dujubinaliyun@users.noreply.github.com>
2021-08-31 00:46:28 +0800

    Update GitTest.java

--- git-test/src/com/bjpowernode/git/GitTest.java
+++ git-test/src/com/bjpowernode/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello kitty");
         System.out.println("hello World");
         System.out.println("hehe");
+        System.out.println("This is a test code");
     }
 }
